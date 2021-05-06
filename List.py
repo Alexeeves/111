179f343b9fa10c2f2ad262778163e730de7e1f10
Alexeeves <alexeeves@gmail.com>
2021-05-06 13:28:29 +0400

    List

--- List.py
+++ List.py
@@ -1,6 +1,6 @@
-list1 = [1, 1, 2, 3, 5, 4, 5, 5, 6]
-list2 = []    
-for i in list1:    
-    if not i in list2:  
-        list2.append(i)
-print (list2)
+a = [1, 1, 2, 3, 5, 4, 5, 5, 6]
+b= []
+for i in a:
+    if not i in b:
+       b.append(i)
+print (b)
